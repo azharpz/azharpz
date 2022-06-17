3d05036cdb8ba489579704a1015d6aac1f49c277
azharpz <you@example.com>
2022-06-18 00:04:46 +0530

    common property file added for mobile

--- src/main/java/resources/MobileData.java
+++ src/main/java/resources/MobileData.java
@@ -3,6 +3,7 @@ package resources;
 import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
 public class MobileData {
@@ -12,24 +13,38 @@ public class MobileData {
     FileInputStream reader;
     Data p;
 	
-	public static Properties getpropertyObject() throws IOException
-	{
+	//public static Properties getpropertyObject() throws IOException
+	//{
 		//reader=new FileInputStream(System.getProperty("user.dir")+"\\EmailLoginTest.properties");
 		//p.load(reader);
 		//p.getProperty("MobileNumber");
 		
 	//return p;
 		
-		FileReader reader=new FileReader("C:\\Users\\user\\eclipse-workspace\\RobustFramework2\\src\\main\\java\\properties\\MobileLoginTest.properties");  
+	//	FileReader reader=new FileReader("C:\\Users\\user\\eclipse-workspace\\RobustFramework2\\src\\main\\java\\properties\\MobileLoginTest.properties");  
 	      
-	   Properties p=new Properties();  
-	   p.load(reader);  
+	//   Properties p=new Properties();  
+	//   p.load(reader);  
 	    
-	    return p;
+	//    return p;
 	      
 	  //  System.out.println(p.getProperty("user"));  
 	   // System.out.println(p.getProperty("password"));  
           
+//}
+	
+	public static Properties getpropertyObject() throws IOException
+	{
+		    Properties p=new Properties();  
+		    String path=System.getProperty("user.dir");
+			InputStream reader=new FileInputStream(path+"\\src\\main\\java\\properties\\MobileLoginTest.properties");  
+
+		   p.load(reader);  
+		    
+		    return p;
+
+          
 }
+	
 	
 }
